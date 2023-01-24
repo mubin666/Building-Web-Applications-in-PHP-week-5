b0a4cf4d11a4070b135bfe2fc27b9295b6a700ba
mubin666 <61820702+mubin666@users.noreply.github.com>
2023-01-25 01:28:25 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<title>Riyan Risky PHP</title>
+<h1>Riyan Risky PHP</h1>
+<p><?php
+	print "The SHA256 hash of \"Riyan Risky\" is ".hash('sha256', 'Riyan Risky');
+?></p>
+<pre>ASCII ART:
+	       ***
+	      *****
+	     **	  **
+	    **     **
+	   ***********
+	  *************
+	 **           **
+	**             **
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
